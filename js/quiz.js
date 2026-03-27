b71a8ee5a415d69c293ea5ae48156b4febb44257
LohnyVal <andreas.lohne.24@gmail.com>
2026-03-27 11:35:36 +0100

    Added styling and added leaderboard to the quiz and localstorage

--- js/quiz.js
+++ js/quiz.js
@@ -230,19 +230,17 @@ const quiz = [
   },
 ];
 
-
 let correctSound = new Audio("../sound/right.mp3");
 let wrongSound = new Audio("../sound/wrong.mp3");
 let doneSound = new Audio("../sound/quizDone.mp3");
-
 
 let isAnswered = false;
 let count = 0;
 let poengSum = 0;
 let wrongSymbol = "❌";
-let correctSymbol = "✅"
+let correctSymbol = "✅";
 
-let medals = ["🥇", "🥈", "🥉", "4", "5"] // Trenger jeg dette?
+let medals = ["🥇", "🥈", "🥉"]; // Første, andre og tredje plass
 
 let nextContainer = document.getElementById("next");
 let h2 = document.getElementById("h2");
@@ -250,26 +248,32 @@ let buttons = document.getElementById("buttons");
 let quizContainer = document.getElementById("quizContainer");
 let summaryContainer = document.getElementById("summaryContainer");
 let videoConatiner = document.getElementById("video");
-let feedback = document.getElementById("feedback")
+let feedback = document.getElementById("feedback");
 let showCurrentQuestion = document.getElementById("showCurrentQuestion");
 
 function startQuiz() {
   let navn = document.getElementById("navn").value;
   document.getElementById("startQuiz").style.display = "none";
   quizContainer.style.display = "flex";
+
+  let person = {
+    navn: navn,
+    poeng: poengSum,
+  };
+
+  localStorage.setItem("bruker", JSON.stringify([person]));
   loadQuiz();
 }
 
 function loadQuiz() {
-
   isAnswered = false;
   let question = quiz[count];
 
   showCurrentQuestion.textContent = `Spørsmål ${count + 1} / ${quiz.length}`;
 
   h2.textContent = question.question;
-  if(question.videoURL){
-   videoConatiner.innerHTML = `
+  if (question.videoURL) {
+    videoConatiner.innerHTML = `
       <video width="400" controls>
         <source src="${question.videoURL}" type="video/mp4">
         Din nettleser støtter ikke video.
@@ -285,11 +289,10 @@ function loadQuiz() {
     const choice = choices[index];
     buttons.innerHTML += `<button id="${index}" onclick="checkAnswer(${choice.correct}, ${index})">${choice.label}</button>`;
   }
-
 }
 
 function checkAnswer(isCorrect, index) {
-  if(isAnswered){
+  if (isAnswered) {
     return;
   }
 
@@ -298,46 +301,70 @@ function checkAnswer(isCorrect, index) {
   isAnswered = true;
 
   const allButtons = buttons.querySelectorAll("button");
-  allButtons.forEach(btn => {
+  allButtons.forEach((btn) => {
     btn.disabled = true;
   });
 
-  
   if (isCorrect) {
-      document.getElementById(index).classList.add("correct");
-      poengSum++;
-      correctSound.currentTime = 0;
-      correctSound.play()
-      feedback.textContent = `Riktig ${correctSymbol}`;
-      nextContainer.innerHTML = `<button onclick="nextQuestion()" id="nextButton">Next</button>`;
-    } else {
-        document.getElementById(index).classList.add("wrong");
-        wrongSound.currentTime = 0;
-        wrongSound.play()
-        feedback.textContent= `Feil ${wrongSymbol}`
-        nextContainer.innerHTML = `<button onclick="nextQuestion()" id="nextButton">Next</button>`;
-    }
+    document.getElementById(index).classList.add("correct");
+    poengSum++;
+    correctSound.currentTime = 0;
+    correctSound.play();
+    feedback.textContent = `Riktig ${correctSymbol}`;
+    nextContainer.innerHTML = `<button onclick="nextQuestion()" id="nextButton">Next</button>`;
+
+    let person = JSON.parse(localStorage.getItem("bruker"));
+    person[0].poeng = poengSum;
+
+    localStorage.setItem("bruker", JSON.stringify(person));
+
+    console.log(person[0]);
+  } else {
+    document.getElementById(index).classList.add("wrong");
+    wrongSound.currentTime = 0;
+    wrongSound.play();
+    feedback.textContent = `Feil ${wrongSymbol}`;
+    nextContainer.innerHTML = `<button onclick="nextQuestion()" id="nextButton">Next</button>`;
+  }
 }
 
 function nextQuestion() {
-    count++;
+  count++;
   let isLastQuestion = quiz.length;
   console.log(count);
   nextContainer.innerHTML = "";
   buttons.innerHTML = "";
   h2.textContent = "";
-  feedback.textContent = ""
-  if(isLastQuestion <= count) {
+  feedback.textContent = "";
+
+  if (isLastQuestion <= count) {
+    let highscoreList = JSON.parse(localStorage.getItem("highscore")) || [];
+    let currentUser = JSON.parse(localStorage.getItem("bruker"));
+    highscoreList = [...highscoreList, ...currentUser];
+
+    highscoreList.sort((a, b) => b.poeng - a.poeng);
+
+    localStorage.setItem("highscore", JSON.stringify(highscoreList));
+    let highscoreDOM = document.getElementById("highscore");
+    console.log(highscoreList, highscoreDOM)
+    highscoreList.forEach((highscore, index) => {
+      const rankLabel = index < medals.length ? medals[index] : index + 1;
+      highscoreDOM.innerHTML += `
+    <li>
+       <span>${rankLabel}: ${highscore.navn}</span>
+       <span id="highscoreText">${highscore.poeng}</span>
+    </li>
+`;
+    });
+
     doneSound.currentTime = 0;
-    doneSound.play()
+    doneSound.play();
     summaryContainer.style.display = "flex";
-    document.getElementById("points").textContent = `Du fikk totalt ${poengSum} av ${quiz.length} poeng`
+    document.getElementById("points").textContent =
+      `Du fikk totalt ${poengSum} av ${quiz.length} poeng`;
 
-    quizContainer.style.display ="none";
+    quizContainer.style.display = "none";
   } else {
-      loadQuiz();
-
+    loadQuiz();
   }
-
-
 }
